7f2763fe14580796901d4760f76d1f62548547d2
Sungwoo, Koo <sungwookoo.dev@gmail.com>
2022-11-14 22:02:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 125 ms, Memory: 29,176 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -2,15 +2,22 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-import javax.security.auth.x500.X500Principal;
-
 public class Solution {
 
+	// 상 -> 하 -> 좌 -> 우
 	static int[] dirX = { -1, 1, 0, 0 };
 	static int[] dirY = { 0, 0, -1, 1 };
 
+	/*
+	 * N: 지도 한 변의 길이 
+	 * K: 최대 공사 가능 깊이 
+	 * map: 지도 
+	 * max: 최대 길이 
+	 * top: 가장 높은 봉우리 높이
+	 */
 	static int N, K, map[][], max, top;
-	static boolean visited[][];
+	
+	static boolean visited[][]; // DFS 방문 체크 배열
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,9 +43,12 @@ public class Solution {
 
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
+					// 가장 높은 봉우리에서만 DFS 진입
 					if (map[i][j] == top) {
+						// 방문 배열 초기화
 						visited = new boolean[N][N];
-                        visited[i][j] = true;
+						// 시작 지점 방문 체크
+						visited[i][j] = true;
 						dfs(i, j, 1, false);
 					}
 				}
@@ -50,55 +60,87 @@ public class Solution {
 		System.out.println(sb);
 	}
 
+	/*
+	 * x, y: 열, 행
+	 * depth: DFS 깊이 (연결한 지형 길이)
+	 * flag: 공사 여부 (지형을 깎는 공사는 단 한 번만 가능하다)
+	 */
 	static void dfs(int x, int y, int depth, boolean flag) {
+		// 현재 depth가 최대 길이라면 max 갱신
 		max = Math.max(max, depth);
-		
-		
 
+		// 상하좌우 이동
 		for (int d = 0; d < 4; d++) {
 			int dx = x + dirX[d];
 			int dy = y + dirY[d];
-			
+
+			// 지도를 벗어나거나 이미 방문한 지형이라면 continue
 			if (isOut(dx, dy) || visited[dx][dy])
 				continue;
-			
-			// 깎기 전 일 경우
+
+			// 지형을 깎기 전(공사 전) 일 경우
 			if (!flag) {
-				// 더 낮은 곳이면 (깎을필요X)
+				// 이동할 곳의 지형의 높이가 더 낮은 경우 (지형을 깎을 필요가 없음)
 				if (map[dx][dy] < map[x][y]) {
+					// 방문 체크
 					visited[dx][dy] = true;
+					// 현재 공사 여부(flag) 그대로 다음 지형으로 이동
 					dfs(dx, dy, depth + 1, flag);
+					// 방문 여부 원상복구
 					visited[dx][dy] = false;
 				}
-				// 같은 곳이면
+				// 이동할 곳의 지형과 높이가 같다면 (지형을 깎아야함)
 				else if (map[dx][dy] == map[x][y]) {
+					// 깎기(공사)전 높이 저장
 					int temp = map[dx][dy];
 					
+					// 방문 체크
 					visited[dx][dy] = true;
+					// 지형 깎기(공사)
 					map[dx][dy] = map[x][y] - 1;
+					
+					// 공사 여부 true로 변경 후 다음 지형으로 이동
 					dfs(dx, dy, depth + 1, true);
+					
+					// 방문 여부 원상복구
 					visited[dx][dy] = false;
+					// 깎기(공사)전 높이로 원상복구
 					map[dx][dy] = temp;
 				}
-				// 높은 곳이면
+				
+				// 이동할 곳의 지형의 높이보다 더 높은 곳이면
 				else {
-					// 깎아서 연결할 수 있을 경우
+					// 최대 K만큼 깎았을 때 연결이 가능하다면 (현재 지형과 이동할 지형의 차보다 K가 더 크다면)
 					if (Math.abs(map[dx][dy] - map[x][y]) < K) {
+						// 깎기(공사)전 높이 저장
 						int temp = map[dx][dy];
+						
+						// 방문 체크
 						visited[dx][dy] = true;
+						
+						// 지형 깎기(공사)
 						map[dx][dy] = map[x][y] - 1;
+						
+						// 공사 여부 true로 변경 후 다음 지형으로 이동
 						dfs(dx, dy, depth + 1, true);
+						
+						// 방문 여부 원상복구
 						visited[dx][dy] = false;
+						// 깎기(공사)전 높이로 원상복구
 						map[dx][dy] = temp;
 					}
 				}
 			}
 
-			// 이미 깎은 경우
+			// 지형을 깎은 후(공사 후)일 경우
 			else {
+				// 이동할 곳의 지형의 높이가 더 낮을 경우
 				if (map[dx][dy] < map[x][y]) {
+					// 방문 체크
 					visited[dx][dy] = true;
-					dfs(dx, dy, depth + 1, true);
+					// 다음 지형으로 이동
+					dfs(dx, dy, depth + 1, flag);
+					// 방문 여부 원상 복구
 					visited[dx][dy] = false;
 				}
 			}
