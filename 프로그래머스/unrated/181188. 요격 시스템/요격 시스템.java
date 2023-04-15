7e7a23a8981a17df5099bd3b935ea8fe2db5ba0b
Sungwoo, Koo <sungwookoo.dev@gmail.com>
2023-04-15 23:00:14 +0900

    [unrated] Title: 요격 시스템, Time: 0.68 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        Arrays.sort(targets, (o1, o2) -> (o1[1] - o2[1]));
+        
+        int end = targets[0][1];
+        for(int[] target : targets) {
+            int s = target[0];
+            int e = target[1];
+            
+            if(end <= s) {
+                end = e;
+                answer++;
+            }
+            
+        }
+                    
+        return answer+1;
+    }
+}
