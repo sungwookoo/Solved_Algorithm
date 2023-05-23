b4426b011e34f097e95d621eb5360d91e75dc239
Sungwoo, Koo <sungwookoo.dev@gmail.com>
2023-05-23 17:03:53 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.05 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
+++ 프로그래머스/lv3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
@@ -1,73 +1,123 @@
 import java.util.*;
-
 class Solution {
-    static int[][] originMap;
-    static int[][] targetMap;
-    static int N, M, answer;
-
+    static int[][] oMap;
+    static int[][] tMap;
+    static int[][] cMap;
+    static String[][] visited;
+    static int N, M, ans;
+    
     public int solution(int[][] beginning, int[][] target) {
+        // 동전을 뒤집으려면, 같은 줄에 있는 모든 동전들을 뒤집어야함.
         N = beginning.length;
         M = beginning[0].length;
-
-        originMap = beginning;
-        targetMap = target;
-
-        // 초기 답을 최대로 설정
-        answer = Integer.MAX_VALUE;
-
-        // 각 행에 대해 뒤집을 것인지 여부를 결정
-        for (int i = 0; i < (1 << N); i++) {
-            // 각 열에 대해 뒤집을 것인지 여부를 결정
-            for (int j = 0; j < (1 << M); j++) {
-                int cnt = 0;
-                // 새로운 상태를 저장할 배열 생성
-                int[][] newMap = new int[N][M];
-
-                // 기존 배열 복사
-                for (int k = 0; k < N; k++)
-                    newMap[k] = Arrays.copyOf(originMap[k], M);
-
-                // 행에 대해 뒤집는 연산 수행
-                for (int k = 0; k < N; k++) {
-                    if (((1 << k) & i) > 0) {
-                        cnt++;
-                        flipRow(newMap, k);
-                    }
-                }
-
-                // 열에 대해 뒤집는 연산 수행
-                for (int k = 0; k < M; k++) {
-                    if (((1 << k) & j) > 0) {
-                        cnt++;
-                        flipColumn(newMap, k);
-                    }
-                }
-
-                // 새로운 상태와 목표 상태가 같다면 답 업데이트
-                if (isSame(newMap, targetMap))
-                    answer = Math.min(answer, cnt);
+        
+        oMap = beginning;
+        tMap = target;
+        cMap = new int[N][M];
+        visited = new String[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                visited[i][j] = "";
+            }
+        }       
+    
+        int answer = -1;
+        
+        for(int i=0; i<N; i++) {
+            dfs(1,i,0,1);
+        }
+        for(int j=0; j<M; j++) {
+            dfs(0,0,j,1);
+        }
+        ans = Integer.MAX_VALUE;
+        
+        if(ans != Integer.MAX_VALUE) answer = ans;
+        
+        return answer;
+    }
+    
+    static void dfs(int mode, int x, int y, int count) {
+        
+        if(isOut(x, y)) {
+            // System.out.println(x+","+y+" 아웃");
+            return;
+        }
+        if(visitCheck(x, y)) {
+            // System.out.println(x+","+y+" already visit ");
+            return;
+        }
+        
+               
+        // 뒤집기
+        if(mode == 1) {
+            reverse(1, x, y);
+        }
+        else reverse(0, x, y);
+                
+        visited[x][y] = mapToString(cMap);
+        
+        if(check()) {
+            ans = Math.min(ans, count);
+            recovery();
+            return;
+        }
+        // 가로
+        dfs(1, x + 1, y, count + 1);
+        // 세로
+        dfs(0, x, y + 1, count + 1);
+        
+    }
+    
+    static boolean isOut(int x, int y) {
+        return x < 0 || y < 0 || x > N-1 || y > M-1;
+    }
+    
+    static boolean visitCheck(int x, int y) {
+        return visited[x][y].equals(mapToString(cMap));
+    }
+    
+    static String mapToString(int[][] map) {
+        String str = "";
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                str += map[i][j];
             }
         }
-
-        return answer == Integer.MAX_VALUE ? -1 : answer;
+        
+        return str;
     }
-
-    void flipRow(int[][] map, int row) {
-        for (int j = 0; j < M; j++)
-            map[row][j] = map[row][j] == 0 ? 1 : 0;
-    }
-
-    void flipColumn(int[][] map, int column) {
-        for (int i = 0; i < N; i++)
-            map[i][column] = map[i][column] == 0 ? 1 : 0;
-    }
-
-    boolean isSame(int[][] map1, int[][] map2) {
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < M; j++)
-                if (map1[i][j] != map2[i][j])
-                    return false;
-
+    
+                
+    static boolean check() {
+        
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(cMap[i][j] != tMap[i][j]) return false;        
+            }
+        }
+        
         return true;
+    }
+    
+    // mode - 1: 가로 , 0: 세로
+    static void reverse(int mode, int x, int y) {
+        if(mode == 1) {
+            for(int i=0; i<M; i++) {
+                cMap[x][i] = cMap[x][i] == 1?0:1;
+            }
+        }
+        else {
+            for(int i=0; i<N; i++) {
+                cMap[i][y] = cMap[i][y] == 1?0:1;
+            }
+        }
+    }
+    
+    static void recovery() {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                cMap[i][j] = oMap[i][j];
+            }
+        }
     }
 }
