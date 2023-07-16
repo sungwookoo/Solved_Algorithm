12434fe2532ceae8be7b87eb589836f3b621d6c6
Sungwoo, Koo <sungwookoo.dev@gmail.com>
2023-07-16 20:05:56 +0900

    [Silver II] Title: 외판원 순회 2, Time: 300 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,63 +1,54 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.DateFormatSymbols;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
-
 public class Main {
-
+	
 	static int N;
-	static int[][] arr;
+	static int[][] w;
 	static boolean[] visited;
 	static int ans;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N][N];
+		
+		StringTokenizer st; 
+		
+		w = new int[N][N];
 		
 		ans = Integer.MAX_VALUE;
-
-		for (int i = 0; i < N; i++) {
+		
+		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+			for(int j=0; j<N; j++) {
+				w[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		for (int i = 0; i < N; i++) {
+		
+		for(int i=0; i<N; i++) {
 			visited = new boolean[N];
 			visited[i] = true;
 			dfs(i, i, 0, 0);
 		}
-
-		System.out.println(ans);
-
-	}
-
-	static void dfs(int start, int now, int sum, int count) {
 		
-		if (count == N - 1) {
-			if (arr[now][start] != 0) {
-				sum += arr[now][start];
-				if (sum < ans) {
-					ans = sum;
-				}
+		System.out.println(ans);
+	}
+	
+	static void dfs(int start, int now, int index, int sum) {
+		
+		if(index == N-1) {
+			if(w[now][start] != 0) {
+				sum += w[now][start];
+				ans = Math.min(ans, sum);
 			}
 			return;
 		}
-
-		for (int i = 0; i < N; i++) {
-			if (visited[i] || arr[now][i] == 0)
-				continue;
-
+		
+		for(int i=0; i<N; i++) {
+			if(visited[i] || w[now][i] == 0) continue;
 			visited[i] = true;
-			dfs(start, i, sum + arr[now][i], count + 1);
+			dfs(start, i, index + 1, sum + w[now][i]);
 			visited[i] = false;
 		}
 	}
