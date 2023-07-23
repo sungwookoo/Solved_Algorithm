c0692469b1926f035d251aa449be11da14482659
Sungwoo, Koo <sungwookoo.dev@gmail.com>
2023-07-23 23:48:57 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1016 ms, Memory: 70888 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.util.Comparator;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		Integer arr[][] = new Integer[N][2];
+
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr, new Comparator<Integer[]>() {
+			@Override
+			public int compare(Integer[] o1, Integer[] o2) {
+				if(o1[0].equals(o2[0])) {
+					return o1[1] - o2[1];
+				}
+				else {
+					return o1[0] - o2[0];
+				}
+			}
+		});	
+
+		for(int i=0; i<N; i++) {
+			sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
