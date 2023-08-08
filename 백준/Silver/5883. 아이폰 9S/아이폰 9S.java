49028d2979d619f2e40eee7a15f4de256f08978c
Sungwoo, Koo <sungwookoo.dev@gmail.com>
2023-08-08 21:55:51 +0900

    [Silver IV] Title: 아이폰 9S, Time: 280 ms, Memory: 44976 KB -BaekjoonHub

--- 백준/Silver/5883. 아이폰 9S/아이폰 9S.java
+++ 백준/Silver/5883. 아이폰 9S/아이폰 9S.java
@@ -1,9 +1,11 @@
+import java.awt.geom.AffineTransform;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 
 public class Main {
 
@@ -22,7 +24,6 @@ public class Main {
 		int count = 0;
 		int max = 1;
 		for(int b : set) {
-//			System.out.println("["+b+"를 뺐을 경우]");
 			list = new ArrayList<Integer>();
 			for(int i=0; i<N; i++) {
 				if(arr[i] == b) continue;
@@ -30,22 +31,30 @@ public class Main {
 			}
 			
 			count = 1;
-			int pre = list.get(0);
 			
 			for(int i=1; i<list.size(); i++) {
-				if(pre != list.get(i)) {
-					count = 1;
-				}
-				else {
-					count++;
-					max = Math.max(max, count);
-				}
-				
-				pre = list.get(i);
+ 				if(list.get(i-1).equals(list.get(i))) {
+ 					
+ 					count ++;
+ 					
+ 					max = Math.max(max, count);
+ 					
+ 				}
+ 				else {
+ 					count = 1;
+ 				}
 			}
+			
 			
 		}
 		System.out.println(max);
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
