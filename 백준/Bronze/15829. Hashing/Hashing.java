b069554b39a73ceb9b20bc1bb7bb4569ecc97c54
Sungwoo, Koo <sungwookoo.dev@gmail.com>
2024-01-05 22:09:29 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14380 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,23 +1,24 @@
 import java.io.*;
+import java.math.BigInteger;
 
 public class Main {
 
-    static int a;
+    static int a, b, v;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long ans = 0;
+        
         a = Integer.parseInt(br.readLine());
         char[] arr = br.readLine().toCharArray();
-        int r = 31;
+        
+        BigInteger ans = new BigInteger("0");
         for (int i = 0; i < arr.length; i++) {
-            ans += ((int) arr[i] - 96) * Math.pow(r, i);
+            ans = ans.add(BigInteger.valueOf(arr[i] - 96).multiply(BigInteger.valueOf(31).pow(i)));
         }
-
+        
         StringBuilder sb = new StringBuilder();
-        sb.append(ans);
+        sb.append(ans.remainder(BigInteger.valueOf(1234567891)));
 
         System.out.println(sb);
     }
-
 }
