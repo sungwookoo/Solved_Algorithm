e77d649d2502d4955b353425fd8e142578f91f78
Sungwoo, Koo <sungwookoo.dev@gmail.com>
2025-02-03 05:57:49 +0900

    [Silver II] Title: 유기농 배추, Time: 64 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,7 +1,7 @@
 import sys
 from collections import deque
 
-t = int(input())
+t = int(sys.stdin.readline().rstrip())
 dirX = [-1, 1, 0, 0]
 dirY = [0, 0, -1, 1]
 
@@ -24,12 +24,12 @@ def bfs(arr, x, y):
 if __name__ == "__main__":
 
     for _ in range(t):
-        m, n, k = map(int, input().split())
+        m, n, k = map(int, sys.stdin.readline().split())
         arr = [[0] * n for _ in range(m)]
         ans = 0
 
         for _ in range(k):
-            x, y = map(int, input().split())
+            x, y = map(int, sys.stdin.readline().split())
             arr[x][y] = 1
 
         for i in range(m):
@@ -37,4 +37,4 @@ if __name__ == "__main__":
                 if arr[i][j] == 1:
                     bfs(arr, i, j)
                     ans += 1
-        print(ans)
+        sys.stdout.write(str(ans)+'\n')
