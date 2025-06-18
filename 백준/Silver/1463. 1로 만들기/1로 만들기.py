7068628cb3d712ccfdf9a0f8c77f593140c2587b
Sungwoo, Koo <sungwookoo.dev@gmail.com>
2025-06-18 10:05:42 +0900

    [Silver III] Title: 1로 만들기, Time: 244 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,17 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+def solve():
+    dp = [0] * (N + 1)
+    for i in range(2, N + 1):
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+    print(dp[N])
+
+
+if __name__ == "__main__":
+    solve()
